6c0b5c7078a62df9d91d0a0a2f93bc4f1475c34a
Valentin <115867837+ValentinBalachkov@users.noreply.github.com>
2023-05-13 19:09:25 +0700

    Icons,Models, menu fix

--- VikingsProject/Assets/_Vikings/_Scripts/UI/CraftAndBuildingMenu.cs
+++ VikingsProject/Assets/_Vikings/_Scripts/UI/CraftAndBuildingMenu.cs
@@ -47,8 +47,9 @@ namespace Vikings.UI
 
                 var item = Instantiate(_menuElement, _content);
                 _menuElements.Add(item);
-                item.UpdateUI(_storageDatas[i].nameText, _storageDatas[i].description, 0, _storageDatas[i].icon,
+                item.UpdateUI(_storageDatas[i].nameText, _storageDatas[i].description, _storageDatas[i].CurrentLevel + 1, _storageDatas[i].icon,
                     _storageDatas[i].PriceToUpgrade);
+                item.SetButtonDescription(_storageDatas[i].CurrentLevel == 0);
                 item.SetEnable((_craftingTableData.currentLevel - _storageDatas[i].CurrentLevel == k) ||
                                (_storageDatas[i].isDefaultOpen && _storageDatas[i].CurrentLevel == 0));
                 var index = i;
@@ -73,8 +74,9 @@ namespace Vikings.UI
 
             var craftingTable = Instantiate(_menuElement, _content);
             _menuElements.Add(craftingTable);
-            craftingTable.UpdateUI(_craftingTableData.nameText, _craftingTableData.description, 0,
+            craftingTable.UpdateUI(_craftingTableData.nameText, _craftingTableData.description, _craftingTableData.currentLevel + 1,
                 _craftingTableData.icon, _craftingTableBuildingData.PriceToUpgrades);
+            craftingTable.SetButtonDescription(_craftingTableData.currentLevel == 0);
             craftingTable.SetEnable(_weaponsOnMapController.WeaponsData[0].IsOpen);
             if (_craftingTableData.currentLevel == 0)
             {
@@ -99,9 +101,10 @@ namespace Vikings.UI
                 var weapon = Instantiate(_menuElement, _content);
                 _menuElements.Add(weapon);
                 weapon.UpdateUI(_weaponsOnMapController.WeaponsData[i].nameText,
-                    _weaponsOnMapController.WeaponsData[i].description, 0,
+                    _weaponsOnMapController.WeaponsData[i].description, 1,
                     _weaponsOnMapController.WeaponsData[i].icon,
                     _weaponsOnMapController.WeaponsData[i].PriceToBuy.ToArray());
+                weapon.SetButtonDescription(!_weaponsOnMapController.WeaponsData[i].IsOpen);
                 weapon.SetEnable(((i == 0 && _storageDatas[0].CurrentLevel > 0) ||
                                  (i == 1 && _craftingTableData.currentLevel == 2)) && !_weaponsOnMapController.WeaponsData[i].IsOpen);
                 var index = i;
--- VikingsProject/Assets/_Vikings/_Scripts/UI/MenuElement.cs
+++ VikingsProject/Assets/_Vikings/_Scripts/UI/MenuElement.cs
@@ -16,6 +16,8 @@ namespace Vikings.UI
         [SerializeField] private Button _upgradeBtn;
         [SerializeField] private Image[] _priceForUpgradeImage;
         [SerializeField] private TMP_Text[] _priceForUpgradeCount;
+        [SerializeField] private TMP_Text _buttonDescription;
+        
 
         [SerializeField] private Sprite _activeSprite;
         [SerializeField] private Sprite _defaultSprite;
@@ -25,13 +27,18 @@ namespace Vikings.UI
         {
             _name.text = itemName;
             _description.text = description;
-          //  _level.text = $"lvl:{level}";
+            _level.text = $"lvl:{level}";
             _icon.sprite = icon;
             for (int i = 0; i < _priceForUpgradeImage.Length; i++)
             {
                 _priceForUpgradeImage[i].sprite = priceToUpgrades[i].itemData.icon;
                 _priceForUpgradeCount[i].text = priceToUpgrades[i].count.ToString();
             }
+        }
+
+        public void SetButtonDescription(bool isCreate)
+        {
+            _buttonDescription.text = isCreate ? "Create" : "Upgrade";
         }
 
         public void SetEnable(bool isEnable)
