dbcd2c1c42d7498e6e297e542daf3f14472dcafb
Valentin <115867837+ValentinBalachkov@users.noreply.github.com>
2023-05-13 12:41:40 +0700

    NewModels

--- VikingsProject/Assets/_Vikings/_Scripts/UI/MenuButtonsManager.cs
+++ VikingsProject/Assets/_Vikings/_Scripts/UI/MenuButtonsManager.cs
@@ -6,16 +6,11 @@ public class MenuButtonsManager : MonoBehaviour
     [SerializeField] private Button _craftButton;
     [SerializeField] private Button _characterButton;
 
-    [SerializeField] private Sprite _activeSprite;
-    [SerializeField] private Sprite _defaultSprite;
-    
 
     public void EnableButtons(bool isEnable)
     {
         _craftButton.interactable = isEnable;
-        _craftButton.image.sprite = isEnable ? _activeSprite : _defaultSprite;
-        _characterButton.interactable = isEnable;
-        _characterButton.image.sprite = isEnable ? _activeSprite : _defaultSprite;
+        _characterButton.interactable = false;
     }
     
 }
