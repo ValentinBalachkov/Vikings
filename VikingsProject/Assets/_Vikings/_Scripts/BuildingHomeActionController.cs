1863dda84d6ed7c9d26b3176d37d18b0f0919190
Valentin_B <valentinbalachkov@yandex.ru>
2023-05-18 15:33:14 +0700

    FixCamera

--- VikingsProject/Assets/_Vikings/_Scripts/BuildingHomeActionController.cs
+++ VikingsProject/Assets/_Vikings/_Scripts/BuildingHomeActionController.cs
@@ -42,7 +42,7 @@ namespace Vikings.Building
 
         private IEnumerator MoveCameraCoroutine(HouseCameraPositionInfo positionInfo)
         {
-            _camera.transform.position = positionInfo.position;
+            _camera.transform.localPosition = positionInfo.position;
             while (_camera.orthographicSize < positionInfo.size)
             {
                 _camera.orthographicSize += 0.01f;
