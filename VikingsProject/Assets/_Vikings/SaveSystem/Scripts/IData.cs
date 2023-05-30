bf5f8ce7c91b8d94bfe6399c752337d7dafa448c
Valentin_B <115867837+ValentinBalachkov@users.noreply.github.com>
2023-05-30 16:10:44 +0700

    ClearOldProject

--- VikingsProject/Assets/_Vikings/SaveSystem/Scripts/IData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SecondChanceSystem.SaveSystem
-{
-    public interface IData //реализуется у ScriptableObject, которые должны быть сохранены
-    {
-        void Save(); //Метод сохранения
-        void Load(); //Метод сохранения
-    }
-}
