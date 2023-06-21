884b861399c3d174003fddcc7845dec8534eca32
Valentin_B <124259010+ValentinMSOne@users.noreply.github.com>
2023-06-21 18:48:23 +0700

    FixMenu

--- Vikings/Assets/_Vikings/_Scripts/UI/MenuButtonsManager.cs
+++ Vikings/Assets/_Vikings/_Scripts/UI/MenuButtonsManager.cs
@@ -4,13 +4,11 @@ using UnityEngine.UI;
 public class MenuButtonsManager : MonoBehaviour
 {
     [SerializeField] private Button _craftButton;
-    [SerializeField] private Button _characterButton;
 
 
     public void EnableButtons(bool isEnable)
     {
         _craftButton.interactable = isEnable;
-        _characterButton.interactable = isEnable;
     }
     
 }
